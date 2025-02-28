6580dbe6dc3e859c9be6dad6f5a7d2e5fb727e8c
Mordorlen <pdrome59@gmail.com>
2025-02-28 14:33:59 +0100

    Koniec zadań z warunków

--- Zadanie 1/Program.cs
+++ Zadanie 1/Program.cs
@@ -10,6 +10,20 @@ namespace Zadanie_1
     {
         static void Main(string[] args)
         {
+            // Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+            // liczba parzysta, czy też nieparzysta
+
+            Console.WriteLine("Podaj liczbe");
+            if (int.Parse(Console.ReadLine()) % 2 == 0)
+            {
+                Console.WriteLine("liczba jest parzysta");
+            }
+            else
+            {
+                Console.WriteLine("liczba jest nieparzysta");
+
+            }
+            Console.ReadKey();
         }
     }
 }
