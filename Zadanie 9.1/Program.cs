0469fb1579e461a443795fd76d212260a8feced4
Mordorlen <pdrome59@gmail.com>
2025-05-08 20:18:15 +0200

    Koniec

---
+++ Zadanie 9.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_9._1
+{
+    class Program
+    {
+        //Zdefiniuj strukturę o nazwie Osoba posiadającą trzy pola: imie, nazwisko i rok_urodzenia.
+        //Zadeklaruj zmienną typu strukturalnego o nazwie o, wczytaj do niej wszystkie wartości pól od
+        //użytkownika i wypisz wszystkie pobrane wartości pól na ekranie.
+        struct Osoba
+        {
+            public string imie;
+            public string nazwisko;
+            public int rok_urodzenia;
+            public Osoba(string imie, string nazwisko, int rok_urodzenia)
+            {
+                this.imie = imie;
+                this.nazwisko = nazwisko;
+                this.rok_urodzenia = rok_urodzenia;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imię:");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko:");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine("Podaj rok urodzenia:");
+            int rokUrodzenia = int.Parse(Console.ReadLine());
+            Osoba o = new Osoba(imie, nazwisko, rokUrodzenia);
+            Console.WriteLine($"Imię: {o.imie}, Nazwisko: {o.nazwisko}, Rok urodzenia: {o.rok_urodzenia}");
+        }
+
+
+    }
+}
---
+++ Zadanie 9.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_9._2
+{
+    class Program
+    {
+        //Zdefiniuj strukturę o nazwie Uczen zawierającą pola: imie, nazwisko i tablice ocen.
+        //Pobierz od użytkownika liczbę uczniów i zadeklaruj odpowiednią tablicę struktur
+        //umożliwiającą przechowanie informacji o imionach, nazwiskach i ocenach uczniów.
+        //Następnie pobierz dane od użytkownika i w sposób czytelny wypisz wszystko na ekranie.
+        struct Uczen
+        {
+            public string imie;
+            public string nazwisko;
+            public int[] oceny;
+            public Uczen(string imie, string nazwisko, int[] oceny)
+            {
+                this.imie = imie;
+                this.nazwisko = nazwisko;
+                this.oceny = oceny;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę uczniów:");
+            int liczbaUczniow = int.Parse(Console.ReadLine());
+            Uczen[] uczniowie = new Uczen[liczbaUczniow];
+            for (int i = 0; i < liczbaUczniow; i++)
+            {
+                Console.WriteLine($"Podaj imię ucznia {i + 1}:");
+                string imie = Console.ReadLine();
+                Console.WriteLine($"Podaj nazwisko ucznia {i + 1}:");
+                string nazwisko = Console.ReadLine();
+                Console.WriteLine($"Podaj liczbę ocen ucznia {i + 1}:");
+                int liczbaOcen = int.Parse(Console.ReadLine());
+                int[] oceny = new int[liczbaOcen];
+                for (int j = 0; j < liczbaOcen; j++)
+                {
+                    Console.WriteLine($"Podaj ocenę {j + 1} ucznia {i + 1}:");
+                    oceny[j] = int.Parse(Console.ReadLine());
+                }
+                uczniowie[i] = new Uczen(imie, nazwisko, oceny);
+            }
+            Console.WriteLine("\nDane uczniów:");
+            foreach (var uczen in uczniowie)
+            {
+                Console.WriteLine($"Imię: {uczen.imie}, Nazwisko: {uczen.nazwisko}, Oceny: {string.Join(", ", uczen.oceny)}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
